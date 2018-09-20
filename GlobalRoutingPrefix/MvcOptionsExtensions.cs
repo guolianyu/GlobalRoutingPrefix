f491d79e036f1e6f564d94a17f999cc1ee29680f
Administrator <admin@example.com>
2018-09-20 14:54:17 +0800

    first commit

---
+++ GlobalRoutingPrefix/MvcOptionsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalRoutingPrefix
+{
+    public static class MvcOptionsExtensions
+    {
+        /// <summary>
+        /// 扩展方法
+        /// </summary>
+        /// <param name="opts"></param>
+        /// <param name="routeAttribute"></param>
+        public static void UseCentralRoutePrefix(this MvcOptions opts, IRouteTemplateProvider routeAttribute)
+        {
+            // 添加我们自定义 实现IApplicationModelConvention的RouteConvention
+            opts.Conventions.Insert(0, new RouteConvention(routeAttribute));
+        }
+    }
+}
---
+++ GlobalRoutingPrefix/RouteConvention.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalRoutingPrefix
+{
+    /// <summary>
+    /// 全局路由前缀配置
+    /// </summary>
+    public class RouteConvention : IApplicationModelConvention
+    {
+
+        /// <summary>
+        /// 定义一个路由前缀变量
+        /// </summary>
+        private readonly AttributeRouteModel _centralPrefix;
+        /// <summary>
+        /// 调用时传入指定的路由前缀
+        /// </summary>
+        /// <param name="routeTemplateProvider"></param>
+        public RouteConvention(IRouteTemplateProvider routeTemplateProvider)
+        {
+            _centralPrefix = new AttributeRouteModel(routeTemplateProvider);
+        }
+
+        //接口的Apply方法
+        public void Apply(ApplicationModel application)
+        {
+            //遍历所有的 Controller
+            foreach (var controller in application.Controllers)
+            {
+                // 已经标记了 RouteAttribute 的 Controller
+                var matchedSelectors = controller.Selectors.Where(x => x.AttributeRouteModel != null).ToList();
+                if (matchedSelectors.Any())
+                {
+                    foreach (var selectorModel in matchedSelectors)
+                    {
+                        // 在 当前路由上 再 添加一个 路由前缀
+                        selectorModel.AttributeRouteModel = AttributeRouteModel.CombineAttributeRouteModel(_centralPrefix,
+                            selectorModel.AttributeRouteModel);
+                    }
+                }
+
+                // 没有标记 RouteAttribute 的 Controller
+                var unmatchedSelectors = controller.Selectors.Where(x => x.AttributeRouteModel == null).ToList();
+                if (unmatchedSelectors.Any())
+                {
+                    foreach (var selectorModel in unmatchedSelectors)
+                    {
+                        // 添加一个 路由前缀
+                        selectorModel.AttributeRouteModel = _centralPrefix;
+                    }
+                }
+            }
+        }
+    }
+}
